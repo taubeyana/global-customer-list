eb938ecb011c962ad82b42a56f3523f2e0eac11c
Yana Taube <taubeyana@gmail.com>
2018-12-18 22:10:54 +0200

    Updates

---
+++ precache-manifest.1e279bcae5c7adc6b242de08d2877cdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d641d9e29ed78cedfea4",
+    "url": "/global-customer-list/static/js/runtime~main.d641d9e2.js"
+  },
+  {
+    "revision": "76f7a161be762dc39742",
+    "url": "/global-customer-list/static/js/main.76f7a161.chunk.js"
+  },
+  {
+    "revision": "cd650794fc076e1f704e",
+    "url": "/global-customer-list/static/js/1.cd650794.chunk.js"
+  },
+  {
+    "revision": "76f7a161be762dc39742",
+    "url": "/global-customer-list/static/css/main.4f261a29.chunk.css"
+  },
+  {
+    "revision": "1465bf7ef885dc8c3cbaea5d9f53838a",
+    "url": "/global-customer-list/index.html"
+  }
+];
